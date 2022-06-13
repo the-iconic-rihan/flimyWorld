153f4be10a731e1b78400333e0264f9fd9f8381c
Rihan Bagwan <rihanbagwan5017@gmail.com>
2022-06-13 22:14:04 +0530

    Modified app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
+// const Person = (props) => {
+//   return (
+//     <>
+//       <h1>Name: {props.name}</h1>
+//       <h2>Last Name: {props.LastName} </h2>
+//       <h3>Age: {props.age}</h3>
+//     </>
+//   )
+// }
+const App = () => {
+  useEffect(() => {
+  
+    alert()
+  })
 
-function App() {
+  // first == name of state setfunction 
+  const [counter, setCounter] = useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((nextCount) => nextCount + 1)}>+</button>
     </div>
   );
 }
 
 export default App;
+
